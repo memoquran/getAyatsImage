26c89660274601fdb843ad7746ec63fd85d292d8
memoquran <88311973+memoquran@users.noreply.github.com>
2021-08-02 18:04:26 +0200

    correct formula

--- function.js
+++ function.js
@@ -6264,7 +6264,7 @@ window.function = function (ayatID, level, end) {
   
   else
   {
-    	var from=salary[ayatID][4]+Math.round(salary[ayatID][5]/levels[level]*Math.ceil(salary[ayatID][6]*levels[level]/salary[ayatID][5]))-1;
+      var from=salary[ayatID][4]+Math.round(salary[ayatID][5]/levels[level]*Math.ceil(salary[ayatID][6]*levels[level]/salary[ayatID][5]-1));
   		var to=salary[ayatID][4]+Math.round(salary[ayatID][5]/levels[level]*Math.ceil(salary[ayatID][6]*levels[level]/salary[ayatID][5]))-1;
     	img_link=img_link+salary[ayatID][2]+"-"+from+"-"+to+".jpg";
   }
