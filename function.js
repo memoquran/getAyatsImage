2e7906c378539325ced84eaddad43f70638536ec
memoquran <88311973+memoquran@users.noreply.github.com>
2021-08-02 16:07:50 +0200

    Update function.js

--- function.js
+++ function.js
@@ -2,6 +2,20 @@
 // have declared in the `glide.json` file, in the
 // same order.
 var fruits = ['Apple', 'Banana'];
+var salary = [
+  [0,0,0,0,0,1,1],
+  [1,1,1,1,1,7,1],
+  [2,1,1,2,1,7,2],
+  [3,1,1,3,1,7,3],
+  [4,1,1,4,1,7,4],
+  [5,1,1,5,1,7,5],
+  [6,1,1,6,1,7,6],
+  [7,1,1,7,1,7,7],
+  [8,2,2,1,1,5,1],
+  [9,2,2,2,1,5,2],
+  [10,2,2,3,1,5,3],
+  [11,2,2,4,1,5,4],
+];
 window.function = function (str, start, end) {
   // For each parameter, its `.value` contains
   // either its value in the type you've declared,
@@ -18,5 +32,5 @@ window.function = function (str, start, end) {
   // error or no result can be produced, because a
   // required input is `undefined`, for example.
   //return str.substring(start, end);
-  return fruits[1];
+  return salary[3][5];
 }
